8b581bd36534e3f88e03e3d457c648b8095636e8
Nishinoyama <begoingto1414@gmail.com>
2023-04-10 16:29:09 +0900

    食品のCRUDのReadとCreateのハンドラー

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/nishinoyama/kobuy-2/ent"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -21,27 +22,78 @@ func main() {
 	}
 	faker.SetGenerateUniqueValues(true)
 	if err := client.User.CreateBulk(
-		client.User.Create().SetName(faker.FirstName()),
-		client.User.Create().SetName(faker.FirstName()),
-		client.User.Create().SetName(faker.FirstName()),
+		client.User.Create().SetName("taro"),
+		client.User.Create().SetName("jiro"),
+		client.User.Create().SetName("saro"),
 	).Exec(context.TODO()); err != nil {
 		log.Fatal(err)
 	}
 	engine := gin.Default()
-	engine.GET("/users", h(client))
-	if err := engine.Run("localhost:3141"); err != nil {
+	v1 := engine.Group("/v1/api")
+	{
+		v1.GET("/users", getUsersHandler(client.User))
+		v1.GET("/groceries", getGroceriesHandler(client.Grocery))
+		v1.POST("/groceries/provide", provideGroceryHandler(client.Grocery))
+	}
+	if err := engine.Run("localhost:8080"); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func h(client *ent.Client) func(ctx *gin.Context) {
-	cctx := context.Background()
-	return func(ctx *gin.Context) {
-		users, err := client.User.Query().All(cctx)
+func getUsersHandler(userClient *ent.UserClient) func(ctx *gin.Context) {
+	return func(gc *gin.Context) {
+		cc := context.Background()
+		users, err := userClient.Query().All(cc)
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, err.Error())
+			gc.JSON(http.StatusInternalServerError, err.Error())
 			return
 		}
-		ctx.JSON(http.StatusOK, users)
+		gc.JSON(http.StatusOK, users)
+	}
+}
+
+func getGroceriesHandler(groceryClient *ent.GroceryClient) func(ctx *gin.Context) {
+	return func(gc *gin.Context) {
+		cc := context.Background()
+		users, err := groceryClient.Query().WithProvider().All(cc)
+		if err != nil {
+			gc.JSON(http.StatusInternalServerError, err.Error())
+			return
+		}
+		gc.JSON(http.StatusOK, users)
+	}
+}
+
+type ProvideGroceryRequest struct {
+	ProviderId     int        `json:"provider_id,omitempty" binding:"required"`
+	Name           string     `json:"name,omitempty" binding:"required"`
+	Price          int        `json:"price,omitempty" binding:"required"`
+	Unit           int        `json:"unit,omitempty" binding:"required"`
+	ExpirationDate *time.Time `json:"expiration_date"`
+}
+
+func provideGroceryHandler(groceryClient *ent.GroceryClient) func(ctx *gin.Context) {
+	return func(gc *gin.Context) {
+		cc := context.Background()
+		var req ProvideGroceryRequest
+		if err := gc.BindJSON(&req); err != nil {
+			gc.JSON(http.StatusBadRequest, err.Error())
+			return
+		}
+		g := groceryClient.Create().
+			SetProviderID(req.ProviderId).
+			SetName(req.Name).
+			SetPrice(req.Price).
+			SetUnit(req.Unit)
+		if req.ExpirationDate != nil {
+			g.SetExpirationDate(*req.ExpirationDate)
+		}
+		if grocery, err := g.Save(cc); err != nil {
+			gc.JSON(http.StatusInternalServerError, err.Error())
+			return
+		} else {
+			gc.JSON(http.StatusOK, grocery)
+			return
+		}
 	}
 }
